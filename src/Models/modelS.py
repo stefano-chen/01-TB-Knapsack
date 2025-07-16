03d53588be648ea3dd8619a34a7def45427d0ddc
stefano-chen <stefanochen01@gmail.com>
2025-07-16 08:13:12 +0200

    model S first implementation

---
+++ src/Models/modelS.py
@@ -0,0 +1,31 @@
+import math
+
+from gurobipy import Model, GRB, quicksum, Env
+
+class ModelS:
+    def __init__(self, *, j, n, c, w, p, q, best_heu_value=0, last_exact_probability=1, previous_solutions=None, ml):
+        self.env = Env(empty=True)
+        self.env.setParam("OutputFlag", 0)
+        self.env.setParam("TimeLimit", ml)
+        self.env.start()
+
+        self.n = n
+        self.model = Model("ModelS", env=self.env)
+
+        self.x = self.model.addVars(n, vtype=GRB.BINARY, name="x")
+
+        self.model.setObjective(quicksum([math.log(q[k])*self.x[k] for k in range(n)]), GRB.MAXIMIZE)
+
+        self.model.addConstr(quicksum([w[k]*self.x[k] for k in range(n)]) <= c)
+
+        self.model.addConstr(quicksum([p[k]*self.x[k] for k in range(n)]) >= (best_heu_value/last_exact_probability)+2.220446049250313e-5)
+
+        for i in range(j):
+            self.model.addConstr(quicksum([self.x[k] if previous_solutions[i][k] else (1-self.x[k]) for k in range(n)]) <= n-1)
+
+    def optimize(self):
+        self.model.optimize()
+        result =  (self.model.Status == GRB.OPTIMAL, [self.x[k].X for k in range(self.n)], self.model.ObjVal)
+        self.model.dispose()
+        self.env.dispose()
+        return result
